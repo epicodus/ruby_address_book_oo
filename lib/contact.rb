6ee339f404622e8e6104a5a26a1b95c626b970d3
Sanjeev Sekar <student@epicodus.com>
2014-08-11 19:56:16 -0700

    Add and complete contact class

--- lib/contact.rb
+++ lib/contact.rb
@@ -25,4 +25,54 @@ class Contact
     @name
   end
 
+  def add_number(number)
+    @number = number
+  end
+
+  def add_numbers(extra)
+    @add_numbers = Number.new(extra)
+  end
+
+  def numbers
+    @add_numbers.all
+  end
+
+  def add_emails(emails)
+    @emails = Email.new(emails)
+  end
+
+  def emails
+    @emails.all
+  end
+
+  def add_addresses(addresses)
+    @addresses = Mail.new(addresses)
+  end
+
+  def addresses
+    @addresses.all
+  end
+
+  def number
+    @number
+  end
+
+  def add_email(email)
+    @email = email
+  end
+
+  def email
+    @email
+  end
+
+  def add_mailing(address)
+    @address = address
+  end
+
+  def mailing
+    @address
+  end
+end
+
+
 
